c0c210d9885f95230e350d5438f0d7adf3034488
EnTex404 <zikov.tosa@gmail.com>
2023-02-27 03:11:33 +0300

    Добавление пакетов

--- Packages/Package.cs
+++ Packages/Package.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Packages
 {
-    internal class Package
+    internal partial class Package
     {
         private Package() { }
         public byte PackageType { get; private set; }
@@ -19,27 +19,5 @@ namespace Packages
         {
             return new Package() { PackageType = type, PackageSubtype = subtype };
         }
-
-        public byte[] ToPackage()
-        {
-            var packet = new MemoryStream();
-
-            packet.Write(ChangeHeaders
-                ? new byte[] { 0x95, 0xAA, 0xFF, PackageType, PackageSubtype } :
-                  new byte[] { 0xAF, 0xAA, 0xAF, PackageType, PackageSubtype }, 0, 5);
-
-            var fields = Fields.OrderBy(f => f.FieldID);
-
-            foreach (var field in fields)
-            {
-                packet.Write(new[] { field.FieldID, field.FieldSize }, 0, 2);
-                packet.Write(field.Contents, 0, field.Contents.Length);
-            }
-
-            packet.Write(new byte[] { 0xFF, 0x00 }, 0, 2);
-
-            return packet.ToArray();
-        }
-
     }
 }
